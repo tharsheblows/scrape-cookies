55769a7c77dca95fdcb8d3dbe6eba517984d8e74
JJ <jjjay@mac.com>
2023-02-11 16:25:05 -0500

    wip: react is working, be nice to auto reload

---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	output: {
+		path: path.join(__dirname, '..', 'public'), // the bundle output path
+		filename: 'bundle.js', // the name of the bundle.
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/, // .js and .jsx files
+				exclude: /node_modules/, // excluding the node_modules folder
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/, // styles files
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+				loader: 'url-loader',
+				options: { limit: false },
+			},
+		],
+	},
+};
